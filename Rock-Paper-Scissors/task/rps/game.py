bbaade7713c335749f5bb3d78d02e91473aae4a3
gaterkik <kik.gater@gmail.com>
2020-11-16 20:20:49 +0200

    fix bags

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -9,21 +9,25 @@ RULES = dict(scissorspaper='win',
              scissorsscissors='draw',
              paperpaper='draw',
              rockrock='draw')
-player_turn = input()
-comp_turn = random.choice(WEAPONS)
+
 # print(comp_turn)
 # print(player_turn+comp_turn)
-result = RULES[player_turn+comp_turn]
+
 # print(result)
 while True:
+    player_turn = input()
+    if player_turn == '!exit':
+        print('Bye!')
+        break
+    elif player_turn not in WEAPONS:
+        print('Invalid input')
+        continue
+    comp_turn = random.choice(WEAPONS)
+    result = RULES[player_turn + comp_turn]
     if result == 'win':
         print('Well done. The computer chose {} and failed'.format(comp_turn))
     elif result == 'loose':
         print('Sorry, but the computer chose {}'.format(comp_turn))
     elif result == 'draw':
         print('There is a draw ({})'.format(comp_turn))
-    elif result == '!exit':
-        print('Bye!')
-        break
-    else:
-        print('Invalid input')
+
