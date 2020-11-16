13ae56239fbe48ad93379dbcc31e7f395c6c5dbf
gaterkik <kik.gater@gmail.com>
2020-11-16 20:02:53 +0200

    add cycle, exit possibility and bad input processing

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -15,9 +15,15 @@ comp_turn = random.choice(WEAPONS)
 # print(player_turn+comp_turn)
 result = RULES[player_turn+comp_turn]
 # print(result)
-if result == 'win':
-    print('Well done. The computer chose {} and failed'.format(comp_turn))
-elif result == 'loose':
-    print('Sorry, but the computer chose {}'.format(comp_turn))
-elif result == 'draw':
-    print('There is a draw ({})'.format(comp_turn))
+while True:
+    if result == 'win':
+        print('Well done. The computer chose {} and failed'.format(comp_turn))
+    elif result == 'loose':
+        print('Sorry, but the computer chose {}'.format(comp_turn))
+    elif result == 'draw':
+        print('There is a draw ({})'.format(comp_turn))
+    elif result == '!exit':
+        print('Bye!')
+        break
+    else:
+        print('Invalid input')
