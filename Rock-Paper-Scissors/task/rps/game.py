cdfc29b8ee782df3dc8a27e64725601d62792b6f
gaterkik <kik.gater@gmail.com>
2020-11-16 19:28:02 +0200

    Initial commit

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,8 @@
+# Write your code here
+player_turn = input()
+if player_turn == 'scissors':
+    print('Sorry, but the computer chose rock')
+elif player_turn == 'paper':
+    print('Sorry, but the computer chose scissors')
+elif player_turn == 'rock':
+    print('Sorry, but the computer chose paper')
